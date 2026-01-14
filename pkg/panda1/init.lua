a9f51678f76420160b3861789b0394698f1f5d4b
胖即是胖 <amaipanda@qq.com>
2026-01-13 19:30:24 -0800

    单挑胖胖

---
+++ pkg/panda1/init.lua
@@ -0,0 +1,12 @@
+local extension = Package:new("panda1")
+extension.extensionName = "panda"
+
+extension:loadSkillSkelsByPath("./packages/panda/pkg/panda1/skills")
+
+Fk:loadTranslationTable{
+  ["panda1"] = "胖胖扩",
+  ["pang"] = "胖",
+}
+
+
+return extension
