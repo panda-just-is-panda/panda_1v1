46066d6545c43066ade0ab054a8b226d3720242c
胖即是胖 <amaipanda@qq.com>
2026-01-14 21:10:15 -0800

    于禁4

--- pkg/panda_one_vs_one1/skills/zhenjun.lua
+++ pkg/panda_one_vs_one1/skills/zhenjun.lua
@@ -6,7 +6,8 @@ local zhenjun = fk.CreateSkill {
 Fk:loadTranslationTable{
   ["pang__zhenjun&"] = "镇军",
   [":pang__zhenjun&"] = "备场技，一名角色登场后，你可以观看其手牌并弃置其中任意张，然后其摸等量张牌。",
-  ["#pang__zhenjun&"] = "镇军：你可以观看 %src 手牌并弃置其中任意张，然后 %src 摸等量张牌",
+  ["#pang__zhenjun"] = "镇军：你可以观看 %src 手牌并弃置其中任意张，然后 %src 摸等量张牌",
+  ["#pang__zhenjun_select"] = "选择其中任意张牌",
 
   ["$pang__zhenjun&1"] = "按丞相之命，此部今由余统摄！",
   ["$pang__zhenjun&2"] = "奉法行令，事上之节，岂有宽宥之理？",
@@ -21,7 +22,7 @@ zhenjun:addEffect(U.AfterDebut,{
   end,
   on_cost = function (self, event, target, player, data)
     return player.room:askToSkillInvoke(player,{
-      prompt = "#pang__zhenjun&::"..target.id,
+      prompt = "#pang__zhenjun:"..target.id,
       skill_name = zhenjun.name,
     })
   end,
@@ -31,6 +32,7 @@ zhenjun:addEffect(U.AfterDebut,{
       target = target,
       min = 0,
       max = 999,
+      prompt = "#pang__zhenjun_select",
       flag = { card_data = { { "$Hand", target.player_cards[Player.Hand] } } },
       skill_name = zhenjun.name,
     })
