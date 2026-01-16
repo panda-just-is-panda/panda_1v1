2cad0998ef27771169de05d9fb45a534b79591d6
胖即是胖 <amaipanda@qq.com>
2026-01-15 18:22:49 -0800

    横江

--- pkg/panda_one_vs_one1/skills/hengjiang.lua
+++ pkg/panda_one_vs_one1/skills/hengjiang.lua
@@ -3,10 +3,10 @@ local hengjiang = fk.CreateSkill({
   tags = {},
 })
 
-local bingxin = {
-  anim_type = "defensive",
+hengjiang:addEffect(fk.Damaged, {
+  anim_type = "control",
   can_trigger = function (self, event, target, player, data)
-    return target == player and player:hasSkill(hengjiang.name) and data.to and data.to:getMaxCards() > 0
+    return player:hasSkill(hengjiang.name) and data.to
   end,
   on_cost = function (self, event, target, player, data)
     return player.room:askToSkillInvoke(player,{
@@ -17,26 +17,25 @@ local bingxin = {
   on_use = function (self, event, target, player, data)
     local room = player.room
     local to = data.to
-    room:addPlayerMark(to, MarkEnum.MinusMaxCards, 1)
-    local choices = {"hengjiang_prevent", "hengjiang__drawcard"}
-    local choice = room:askToChoice(player, {
-      choices = choices,
-      skill_name = hengjiang.name,
+    local cards = room:askToDiscard(to, {
+          skill_name = hengjiang.name,
+          cancelable = true,
+          min_num = 2,
+          max_num = 2,
+          include_equip = false,
     })
-    if choice == "hengjiang_prevent" then
-        data:preventDamage()
-    else
-        to:drawCards(2, hengjiang.name)
+    if #cards == 2 then
+      room:addPlayerMark(to, MarkEnum.AddMaxCards, 1)
     end
-  end
-}
-hengjiang:addEffect(fk.DamageCaused, bingxin)
-hengjiang:addEffect(fk.DamageInflicted, bingxin)
+    room:addPlayerMark(to, MarkEnum.MinusMaxCards, 1)
+    to:drawCards(2, hengjiang.name)
+  end,
+})
 
 
 Fk:loadTranslationTable {["pang__hengjiang"] = "横江",
-[":pang__hengjiang"] = "当你造成或受到伤害时，若受伤角色的手牌上限大于0，你可以令其手牌上限-1，然后你选择一项：其摸两张牌；防止此伤害。",
-["#pang__hengjiang"] = "横江：你可以令 %src 手牌上限-1，然后选择防止此伤害或令 %src 摸两张牌",
+[":pang__hengjiang"] = "当一名角色受到伤害后，你可以令其摸两张牌，然后手牌上限-1；其可以先弃置两张手牌，然后手牌上限+1。",
+["#pang__hengjiang"] = "横江：你可以令 %src 摸两张牌且手牌上限-1，%src 可以先弃置两张牌并令手牌上限+1",
 
 ["hengjiang_prevent"] = "防止伤害",
 ["hengjiang__drawcard"] = "摸两张牌",
