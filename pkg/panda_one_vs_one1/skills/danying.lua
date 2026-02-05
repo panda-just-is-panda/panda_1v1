fbabfe6dff51b0924a92416332bb62d7171cb13f
胖即是胖 <amaipanda@qq.com>
2026-02-05 11:19:38 -0800

    周善1

---
+++ pkg/panda_one_vs_one1/skills/danying.lua
@@ -0,0 +1,80 @@
+local danying = fk.CreateSkill {
+  name = "pang__danying",
+}
+
+Fk:loadTranslationTable{
+  ["pang__danying"] = "胆迎",
+  [":pang__danying"] = "你可以视为使用【杀】或【闪】，然后下次有角色使用另一牌名的牌时，其可以对你造成2点伤害。",
+
+  ["#pang__danying"] = "胆迎：你可以视为使用【杀】或【闪】",
+  ["#pang__danying_damage"] = "胆迎：你可以对 %src 造成2点伤害",
+
+  ["$pang__danying1"] = "早就想会会你常山赵子龙了。",
+  ["$pang__danying2"] = "赵子龙是吧？兜鍪给你打掉。",
+}
+
+danying:addEffect("viewas", {
+  pattern = "slash,jink",
+  prompt = "#danying",
+  interaction = function(self, player)
+    local all_names = {"slash", "jink"}
+    local names = player:getViewAsCardNames(danying.name, all_names)
+    if #names > 0 then
+      return UI.CardNameBox {choices = names, all_choices = all_names}
+    end
+  end,
+  filter_pattern = {
+    min_num = 0,
+    max_num = 0,
+    pattern = "",
+    subcards = {}
+  },
+  view_as = function(self, player, cards)
+    if self.interaction.data == nil then return end
+    local card = Fk:cloneCard(self.interaction.data)
+    card.skillName = danying.name
+    return card
+  end,
+  before_use = function(self, player, use)
+    local name = Fk:cloneCard(self.interaction.data)
+    if name == "slash" then
+        player.room:setPlayerMark(player,"pang_danding_jink",1)
+    else
+        player.room:setPlayerMark(player,"pang_danding_slash",1)
+    end
+  end,
+  enabled_at_play = function(self, player)
+    return true
+  end,
+  enabled_at_response = function(self, player, response)
+    return not response
+  end,
+})
+
+danying:addEffect(fk.CardUsing, {
+  can_refresh = function(self, event, target, player, data)
+    return target and data.card and player:hasSkill(danying.name) 
+    and (data.card.trueName == "slash" and player:getMark("pang_danding_slash") > 0 
+    or player:getMark("pang_danding_jink") > 0 and data.card.trueName == "jink")
+  end,
+  on_refresh = function(self, event, target, player, data)
+    local room = player.room
+    local to = target
+    room:setPlayerMark(player,"pang_danding_jink",0)
+    room:setPlayerMark(player,"pang_danding_slash",0)
+    if player.room:askToSkillInvoke(to,{
+      prompt = "#pang__danying_damage:".. player.id,
+      skill_name = danying.name,
+    })
+    then
+        room:damage{
+        from = to,
+        to = player,
+        damage = 2,
+        skillName = danying.name,
+    }
+    end
+  end,
+})
+
+return danying
---
+++ pkg/panda_one_vs_one1/skills/miyun.lua
@@ -0,0 +1,75 @@
+local miyun = fk.CreateSkill {
+  name = "pang__miyun",
+}
+
+Fk:loadTranslationTable{
+  ["pang__miyun"] = "密运",
+  [":pang__miyun"] = "当你登场后，你可以摸一张牌并展示之；六轮结束后，你可以展示此牌并令登场数-1，然后你退场。",
+
+  ["#pang__miyun"] = "密运：你可以摸一张牌并展示之",
+  ["#pang__miyun-leave"] = "密运：你可以令登场数-1并退场",
+  ["@@pang__miyun"] = "密运",
+  ["@pang__miyun_count"] = "密运",
+
+
+  ["$pang__miyun1"] = "不要大张旗鼓，要神不知鬼不觉。",
+  ["$pang__miyun2"] = "小阿斗，跟本将军走一趟吧。",
+}
+
+local U = require "packages.klee_fk_B.pkg.gamemode.klee_1v1_util"
+
+miyun:addEffect(U.AfterDebut,{
+  can_trigger = function (self, event, target, player, data)
+    return player:hasSkill(miyun.name) and target == player
+      and not data.begingame
+  end,
+  on_cost = function (self, event, target, player, data)
+    return player.room:askToSkillInvoke(player,{
+      prompt = "#pang__miyun",
+      skill_name = miyun.name,
+    })
+  end,
+  on_use = function (self, event, target, player, data)
+    local room = player.room
+    player:drawCards(1, miyun.name, nil, "@@pang__miyun")
+  end,
+})
+
+miyun:addEffect(fk.RoundEnd,{
+  can_trigger = function (self, event, target, player, data)
+    return player:hasSkill(miyun.name,true)
+  end,
+  on_cost = function (self, event, target, player, data)
+    player.room:addPlayerMark(player,"@pang__miyun_count", 1)
+    if player:getMark("@pang__miyun_count") > 5 and player:hasSkill(miyun.name) then
+        if table.find(player:getCardIds("h"), function (id)
+        return Fk:getCardById(id):getMark("@@pang__miyun") > 0
+      end) and player.room:askToSkillInvoke(player,{
+            prompt = "#pang__miyun-leave".. player.id,
+            skill_name = miyun.name,
+        }) then
+            player.room:setPlayerMark(player,"@pang__miyun_count", 0)
+            return true
+        end
+    end
+  end,
+  on_use = function (self, event, target, player, data)
+    local room = player.room
+    U.addPlayercount(player,1,-1)
+    U.PlayerDebut(player,miyun.name,false)
+  end,
+})
+
+miyun:addLoseEffect(function (self, player, is_death)
+  for _, id in ipairs(player:getCardIds("h")) do
+    player.room:setCardMark(Fk:getCardById(id), "@@pang__miyun", 0)
+  end
+  for _, id in ipairs(player.room.discard_pile) do
+    player.room:setCardMark(Fk:getCardById(id), "@@pang__miyun", 0)
+  end
+  for _, id in ipairs(player.room.draw_pile) do
+    player.room:setCardMark(Fk:getCardById(id), "@@pang__miyun", 0)
+  end
+end)
+
+return miyun
