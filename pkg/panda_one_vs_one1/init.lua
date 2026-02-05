ac1589d8e1fdf8697fb4a94c7f99d77bfb8cd5c5
胖即是胖 <amaipanda@qq.com>
2026-02-05 11:22:27 -0800

    周善2

--- pkg/panda_one_vs_one1/init.lua
+++ pkg/panda_one_vs_one1/init.lua
@@ -46,16 +46,16 @@ Fk:loadTranslationTable{
   ["~pang__sunshangxiang"] = "哎呀，这次弓箭射歪了。",
 }
 
-local panzhangmazhong = General:new(extension, "pang__panzhangmazhong", "wu", 4, 4, General.Male)
-panzhangmazhong:addSkill("pang__anjian&")
-panzhangmazhong:addSkill("pang__duodao")
+local zhoushan = General:new(extension, "pang__zhoushan", "wu", 4, 4, General.Male)
+zhoushan:addSkill("pang__miyun")
+zhoushan:addSkill("pang__danying")
 Fk:loadTranslationTable{
-  ["pang__panzhangmazhong"] = "潘璋马忠",
-  ["#pang__panzhangmazhong"] = "擒龙伏虎",
+  ["pang__panzhangmazhong"] = "周善",
+  ["#pang__panzhangmazhong"] = "荆吴刑天",
   ["designer:pang__panzhangmazhong"] = "胖即是胖",
-  ["illustrator:pang__panzhangmazhong"] = "凝聚永恒",
+  ["illustrator:pang__panzhangmazhong"] = "游漫美绘",
 
-  ["~pang__panzhangmazhong"] = "埋伏得这么好，怎会……",
+  ["~pang__panzhangmazhong"] = "夫人救我！夫人救我！",
 }
 
 local huayun = General:new(extension, "pang__huayun", "wei", 3, 3, General.Male)
