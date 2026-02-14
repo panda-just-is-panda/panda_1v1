763d4f3e3e1c6bf33ed63d1d9e0916ebb9454f1d
胖即是胖 <amaipanda@qq.com>
2026-02-14 04:22:17 -0800

    通渠加强

--- pkg/panda_one_vs_one1/skills/tongqu.lua
+++ pkg/panda_one_vs_one1/skills/tongqu.lua
@@ -4,8 +4,8 @@ local tongqu = fk.CreateSkill {
 
 Fk:loadTranslationTable{
   ["pang__tongqu"] = "通渠",
-  [":pang__tongqu"] = "每局限两次，出牌阶段，你可以弃置四张牌，然后你的摸牌阶段摸牌数+1。",
-  ["#pang__tongqu"] = "通渠：你可以弃置四张牌，然后你的摸牌阶段摸牌数+1",
+  [":pang__tongqu"] = "每局限两次，出牌阶段，你可以弃置三张牌，然后你的摸牌阶段摸牌数+1。",
+  ["#pang__tongqu"] = "通渠：你可以弃置三张牌，然后你的摸牌阶段摸牌数+1",
   ["@tongqu_count"] = "通渠",
 
   ["$pang__tongqu1"] = "兴凿修渠，依水屯军！",
@@ -17,13 +17,13 @@ tongqu:addEffect("active", {
   anim_type = "control",
   prompt = "#pang__tongqu",
   max_phase_use_time = 2,
-  card_num = 4,
+  card_num = 3,
   target_num = 0,
   can_use = function(self, player)
     return player:usedSkillTimes(tongqu.name, Player.HistoryGame) < 2
   end,
   card_filter = function(self, player, to_select, selected)
-    return #selected < 4 and not player:prohibitDiscard(to_select)
+    return #selected < 3 and not player:prohibitDiscard(to_select)
   end,
   target_filter = Util.FalseFunc,
   on_use = function(self, room, effect)
