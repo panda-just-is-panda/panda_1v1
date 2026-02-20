f3000db574bd17dcd15e0e0971e39b93896ac271
胖即是胖 <amaipanda@qq.com>
2026-02-19 17:59:51 -0800

    王元姬1

---
+++ pkg/panda_one_vs_one1/skills/qianchong.lua
@@ -0,0 +1,45 @@
+local qianchong = fk.CreateSkill {
+  name = "pang__qianchong",
+  tags = {Skill.Switch},
+}
+
+qianchong:addEffect(fk.CardUsing, {
+    mute = true,
+  can_trigger = function(self, event, target, player, data)
+    return data.card and player:hasSkill(qianchong.name) and data.card.color == Card.Black
+    (data.card.trueName == "slash" and player:getSwitchSkillState(qianchong.name, true) ~= fk.SwitchYang
+    or data.card.type == Card.TypeTrick and player:getSwitchSkillState(qianchong.name, true) == fk.SwitchYang)
+  end,
+  on_cost = function (self, event, target, player, data)
+    return player.room:askToSkillInvoke(player,{
+    prompt = "#pang__qianchong",
+    skill_name = qianchong.name,
+    })
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    if player:getSwitchSkillState(qianchong.name, true) == fk.SwitchYang then
+        player:broadcastSkillInvoke(qianchong.name, 2)
+    else
+        player:broadcastSkillInvoke(qianchong.name, 1)
+    end
+    data:removeAllTargets()
+  end,
+})
+
+Fk:loadTranslationTable {["pang__qianchong"] = "谦冲",
+[":pang__qianchong"] = "转换技，当一名角色使用①黑色【杀】②黑色锦囊牌时，你可以令此牌无效。",
+
+[":pang__qianchong_yang"] = "转换技，当一名角色使用<font color=\"#E0DB2F\">①黑色【杀】</font>" ..
+"②黑色锦囊牌时，你可以取消之。",
+[":pang__qianchong_yin"] = "转换技，当一名角色使用①黑色【杀】"..
+"<font color=\"#E0DB2F\">②黑色锦囊牌</font>时，你可以取消之。",
+
+["#pang__qianchong"] = "谦冲：你可以取消此牌",
+
+
+["$pang__qianchong1"] = "谦谨行事，方能多吉少恙。",
+  ["$pang__qianchong2"] = "宫闱之内，何必擅摄外事。",
+}
+
+return qianchong
---
+++ pkg/panda_one_vs_one1/skills/shangjian.lua
@@ -0,0 +1,105 @@
+local shangjian = fk.CreateSkill {
+  name = "pang__shangjian",
+}
+
+Fk:loadTranslationTable{
+  ["pang__shangjian"] = "尚俭",
+  [":pang__shangjian"] = "每轮结束时，你可以获得本轮因弃置进入弃牌堆的一张牌，然后对手也可以如此做。",
+
+  ["#pang__shangjian"] = "尚俭：你可以获得本轮因弃置进入弃牌堆的一张牌",
+  ["#pang__shangjian_get"] = "尚俭：获得本轮因弃置进入弃牌堆的一张牌",
+
+
+
+  ["$pang__shangjian1"] = "如今乱世，当秉俭行之节。",
+  ["$pang__shangjian2"] = "百姓尚处寒饥之困，吾等不可奢费财力。",
+}
+
+
+shangjian:addEffect(fk.RoundEnd,{
+    anim_type = "support",
+  can_trigger = function (self, event, target, player, data)
+    if player:hasSkill(shangjian.name) then
+        local room = player.room
+        local cards = {}
+        local shangjian_all = {}
+        room.logic:getEventsByRule(GameEvent.MoveCards, 1, function (e)
+        for _, move in ipairs(e.data) do
+          for _, info in ipairs(move.moveInfo) do
+            local id = info.cardId
+            if not table.contains(cards, id) then
+              table.insert(cards, id)
+              if move.toArea == Card.DiscardPile and move.moveReason == fk.ReasonDiscard and
+                room:getCardArea(id) == Card.DiscardPile then
+                table.insert(shangjian_all, id)
+              end
+            end
+          end
+        end
+        return false
+      end, nil, Player.HistoryRound)
+      if #shangjian_all > 0 then
+        event:setCostData(self, {extra_data = {shangjian_all}})
+        return true
+      end
+    end
+  end,
+  on_cost = function(self, event, target, player, data)
+    local room = player.room
+    if room:askToSkillInvoke(player, {
+      skill_name = shangjian.name,
+      prompt = "#pang__shangjian",
+    }) then
+      event:setCostData(self, {extra_data = event:getCostData(self).extra_data})
+      return true
+    end
+  end,
+  on_use = function (self, event, target, player, data)
+    local room = player.room
+    local shangjian_all = event:getCostData(self).extra_data[1]
+    local player_get = room:askToChooseCards(player, {
+          cards = shangjian_all,
+          min = 1,
+          max = 1,
+          skill_name = shangjian.name,
+          prompt = "#pang__shangjian_get",
+          all_cards = shangjian_all,
+        })
+    local moveInfos = {}
+    table.insert(moveInfos, {
+      ids = player_get,
+      to = player,
+      toArea = Card.PlayerHand,
+      moveReason = fk.ReasonJustMove,
+      proposer = player,
+      skillName = shangjian.name,
+    })
+    table.removeOne(shangjian_all, player_get[1])
+    if #shangjian_all > 0 and room:askToSkillInvoke(player.next, {
+      skill_name = shangjian.name,
+      prompt = "#pang__shangjian",
+    }) then
+        local to_get = room:askToChooseCards(player.next, {
+          cards = shangjian_all,
+          min = 1,
+          max = 1,
+          skill_name = shangjian.name,
+          prompt = "#pang__shangjian_get",
+          all_cards = shangjian_all,
+        })
+        local moveInfos = {}
+        table.insert(moveInfos, {
+            ids = to_get,
+            to = player.next,
+            toArea = Card.PlayerHand,
+            moveReason = fk.ReasonJustMove,
+            proposer = player,
+            skillName = shangjian.name,
+        })
+    end
+    room:moveCards(table.unpack(moveInfos))
+  end,
+})
+
+
+return shangjian
