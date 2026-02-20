fe2a4f843ead3e7d844d2510f03e50008ad92c62
胖即是胖 <amaipanda@qq.com>
2026-02-19 17:09:31 -0800

    王濬2

--- pkg/panda_one_vs_one1/skills/jianshi.lua
+++ pkg/panda_one_vs_one1/skills/jianshi.lua
@@ -4,13 +4,13 @@ local jianshi = fk.CreateSkill {
 
 Fk:loadTranslationTable{
   ["pang__jianshi"] = "舰势",
-  [":pang__jianshi"] = "准备阶段，你可以依次重铸至多两张牌，然后你可以依次使用至多X张因此获得的【杀】（X为你装备区内的牌数）。",
+  [":pang__jianshi"] = "准备阶段，你可以依次重铸至多两张牌，然后你可以依次使用至多X张【杀】（X为你装备区内的牌数）。",
 
   ["#jianshi_recast1"] = "舰势：你可以依次重铸至多两张牌（第1张）",
   ["#jianshi_recast2"] = "舰势：你可以依次重铸至多两张牌（第2张）",
   ["@@jianshi_slash-phase"] = "舰势",
-  ["#jianshi_use1"] = "舰势：你可以使用至多两张因此获得的【杀】（第1张）",
-  ["#jianshi_use2"] = "舰势：你可以使用至多两张因此获得的【杀】（第2张）",
+  ["#jianshi_use1"] = "舰势：你可以使用至多两张【杀】（第1张）",
+  ["#jianshi_use2"] = "舰势：你可以使用至多两张【杀】（第2张）",
 
 
   ["$pang__jianshi1"] =  "修橹筑楼舫，伺时补金瓯。",
@@ -20,6 +20,7 @@ Fk:loadTranslationTable{
 local U = require "packages.klee_fk_B.pkg.gamemode.klee_1v1_util"
 
 jianshi:addEffect(fk.EventPhaseStart, {
+  mute = true,
   can_trigger = function(self, event, target, player, data)
     return target == player and player:hasSkill(jianshi.name) and
       player.phase == Player.Start
@@ -41,7 +42,8 @@ jianshi:addEffect(fk.EventPhaseStart, {
   on_use = function(self, event, target, player, data)
     local room = player.room
     local card1 = event:getCostData(self).cards[1]
-    room:recastCard(card1, player, jianshi.name,nil,"@@jianshi_slash-phase")
+    room:recastCard(card1, player, jianshi.name,nil)
+    player:broadcastSkillInvoke(jianshi.name, 1)
     local card2 = player.room:askToCards(player, {
           skill_name = jianshi.name,
           prompt = "#jianshi_recast2",
@@ -51,28 +53,22 @@ jianshi:addEffect(fk.EventPhaseStart, {
           include_equip = true,
     })
     if #card2 > 0 then
-        room:recastCard(card2, player, jianshi.name,nil,"@@jianshi_slash-phase")
+        room:recastCard(card2, player, jianshi.name)
     end
-    local slashes = table.filter(player:getCardIds("h"),function (element, index, array)
-      return Fk:getCardById(element).trueName == "slash" 
-      and Fk:getCardById(element):getMark("@@jianshi_slash-phase") > 0
-    end)
     if #player:getCardIds("e") > 0 then
+        player:broadcastSkillInvoke(jianshi.name, 2)
         local use = room:askToUseCard(player, {
             skill_name = jianshi.name,
-            pattern = tostring(Exppattern{ id = slashes }),
+            pattern = "slash",
             prompt = "#jianshi_use1",
             extra_data = {
                 bypass_times = true,
             }
         })
-        slashes = table.filter(slashes, function (id)
-            return table.contains(player:getCardIds("h"), id)
-        end)
         if use and #player:getCardIds("e") > 0 then
             local use = room:askToUseCard(player, {
                 skill_name = jianshi.name,
-                pattern = tostring(Exppattern{ id = slashes }),
+                pattern = "slash",
                 prompt = "#jianshi_use2",
                 extra_data = {
                     bypass_times = true,
