ed2576ea1774b4edcb6b3a466e83cdc192f2c595
胖即是胖 <amaipanda@qq.com>
2026-03-01 20:54:44 -0800

    bug修复

--- init.lua
+++ init.lua
@@ -1,14 +1,15 @@
 
 local prefix = "packages.panda_one_vs_one.pkg."
 local panda1 = require "packages.panda_one_vs_one.pkg.panda_one_vs_one1"
-local panda1 = require "packages.panda_one_vs_one.pkg.panda_one_vs_one2"
+local panda2 = require "packages.panda_one_vs_one.pkg.panda_one_vs_one2"
 
 Fk:loadTranslationTable{
   ["panda_one_vs_one"] = "胖胖11",
 }
 
 return{
-   panda1
+   panda1,
+   panda2,
 }
 
 
