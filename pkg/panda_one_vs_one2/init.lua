8ecfd60162fec8a5b55a180946ddb18db2350e31
胖即是胖 <amaipanda@qq.com>
2026-03-03 00:28:39 -0800

    蒋济2

--- pkg/panda_one_vs_one2/init.lua
+++ pkg/panda_one_vs_one2/init.lua
@@ -44,6 +44,7 @@ Fk:loadTranslationTable{
 
   ["~pang__jiangji"] = "洛水之誓，言犹在耳……咳咳咳……",
 }
+jiangji.hidden = true
 
 local zhangmancheng = General:new(extension, "pang__zhangmancheng", "qun", 4, 4, General.Male)
 zhangmancheng:addSkill("pang__zhongji")
@@ -56,6 +57,6 @@ Fk:loadTranslationTable{
 
   ["~pang__zhangmancheng"] = "逡巡不前，坐以待毙。",
 }
-zhangmancheng.hidden = true
+
 
 return extension
