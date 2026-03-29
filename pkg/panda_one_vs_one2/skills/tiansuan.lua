961f3c958bb3bdfc81e67357ee815af27cd4e195
胖即是胖 <amaipanda@qq.com>
2026-03-29 03:19:38 -0700

    周群削弱

--- pkg/panda_one_vs_one2/skills/tiansuan.lua
+++ pkg/panda_one_vs_one2/skills/tiansuan.lua
@@ -4,7 +4,7 @@ local tiansuan = fk.CreateSkill {
 
 Fk:loadTranslationTable{
   ["pang__tiansuan"] = "天算",
-  [":pang__tiansuan"] = "出牌阶段限一次，你可以依次进行两次判定，然后若判定结果均为：红色，你摸一张牌；黑色，你弃置对手一张牌。",
+  [":pang__tiansuan"] = "出牌阶段限一次，你可以进行一次判定，然后若判定结果为：方块，你摸一张牌；黑桃，你弃置对手一张牌。",
   ["#pang__tiansuan"] = "天算：你可以进行两次判定，根据判定结果触发对应效果",
 
   ["$pang__tiansuan1"] = "雷霆雨露，皆为君恩。",
@@ -30,50 +30,27 @@ tiansuan:addEffect("active", {
       who = player,
       reason = tiansuan.name,
       pattern = {
-        [".|.|spade,club,diamond,heart"] = "good",
+        [".|.|spade,diamond"] = "good",
         [".|.|spade"] = "spade",
-        [".|.|club"] = "club",
         [".|.|diamond"] = "diamond",
-        [".|.|heart"] = "heart",
-        ["else"] = "bad"
-      },
-    }
-    local judge2 = {
-      who = player,
-      reason = tiansuan.name,
-      pattern = {
-        [".|.|spade,club,diamond,heart"] = "good",
-        [".|.|spade"] = "spade",
-        [".|.|club"] = "club",
-        [".|.|diamond"] = "diamond",
-        [".|.|heart"] = "heart",
         ["else"] = "bad"
       },
     }
     player:broadcastSkillInvoke(tiansuan.name, 2)
     room:judge(judge1)
     local suit1
-    local suit2
     if not player.dead and judge1.results then
-        if table.contains(judge1.results, "diamond") or table.contains(judge1.results, "heart") then
+        if table.contains(judge1.results, "diamond") then
             suit1 = "red"
-        elseif table.contains(judge1.results, "club") or table.contains(judge1.results, "spade") then
+        elseif table.contains(judge1.results, "spade") then
             suit1 = "black"
         end
     end
-    room:judge(judge2)
-    if not player.dead and judge2.results then
-        if table.contains(judge2.results, "diamond") or table.contains(judge2.results, "heart") then
-            suit2 = "red"
-        elseif table.contains(judge2.results, "club") or table.contains(judge2.results, "spade") then
-            suit2 = "black"
-        end
-    end
-    if suit1 and suit2 then
-        if suit1 == "red" and suit2 == "red" then
+    if suit1 then
+        if suit1 == "red"  then
             player:drawCards(1, tiansuan.name)
             player:broadcastSkillInvoke(tiansuan.name, 1)
-        elseif suit1 == "black" and suit2 == "black" then
+        elseif suit1 == "black"  then
             player:broadcastSkillInvoke(tiansuan.name, 1)
             local card = room:askToChooseCard(player, {
                 target = target,
