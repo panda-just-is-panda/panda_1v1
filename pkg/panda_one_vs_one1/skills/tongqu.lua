c4aec9f58f867910f572543d3f6ce1ec3f281415
胖即是胖 <amaipanda@qq.com>
2026-03-30 17:30:47 -0700

    通渠4

--- pkg/panda_one_vs_one1/skills/tongqu.lua
+++ pkg/panda_one_vs_one1/skills/tongqu.lua
@@ -4,11 +4,11 @@ local tongqu = fk.CreateSkill {
 
 Fk:loadTranslationTable{
   ["pang__tongqu"] = "通渠",
-  [":pang__tongqu"] = "摸牌阶段结束时，你将手牌摸至三张；若你未因此摸牌，你可以弃置一张牌并于本回合的下个阶段结束时发动此技能。",
-  ["#pang__tongqu"] = "通渠：你可以将手牌摸至三张",
+  [":pang__tongqu"] = "摸牌阶段结束时，你可以将手牌摸至三张，或弃置一张牌并于本回合的下个阶段结束时发动此技能。",
+  ["#pang__tongqu"] = "通渠：你可以选择将手牌摸至三张或弃牌",
 
   ["@@pang__tongqu-turn"] = "可通渠",
-  ["#pang__tongqu_discard"] = "通渠：你可以弃置一张牌，于本回合的下个阶段结束时再次发动此技能",
+  ["#pang__tongqu_discard"] = "通渠：你可以直接点确定将手牌摸至三张，或选择一张牌弃置并于本回合的下个阶段结束时发动此技能",
 
   ["$pang__tongqu1"] = "兴凿修渠，依水屯军！",
   ["$pang__tongqu2"] = "开渠疏道，以备军实！",
@@ -20,15 +20,14 @@ tongqu:addEffect(fk.EventPhaseEnd, {
     return target == player and player:hasSkill(tongqu.name) and (player.phase == Player.Draw or player:getMark("@@pang__tongqu-turn") > 0)
   end,
   on_cost = function(self, event, target, player, data)
-    player.room:setPlayerMark(player, "@@jutu", 0)
-    return true 
+    player.room:setPlayerMark(player, "@@pang__tongqu-turn", 0)
+    return player.room:askToSkillInvoke(player, {
+      skill_name = tongqu.name,
+      prompt = "#pang__tongqu",
+    })
   end,
   on_use = function(self, event, target, player, data)
     local room = player.room
-    local X = 3 - player:getHandcardNum()
-    if X > 0 then
-        player:drawCards(X, tongqu.name)
-    end
     local card = room:askToDiscard(player, {
       skill_name = tongqu.name,
       prompt = "#pang__tongqu_discard",
@@ -39,6 +38,11 @@ tongqu:addEffect(fk.EventPhaseEnd, {
     })
     if #card > 0 then
       room:setPlayerMark(player, "@@pang__tongqu-turn", 1)
+    else
+      local X = 3 - player:getHandcardNum()
+      if X > 0 then
+        player:drawCards(X, tongqu.name)
+      end
     end
   end,
 })
