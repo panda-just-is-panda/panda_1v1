2370183604f25f019c0b8ba4e73c29a8bdea8e30
胖即是胖 <amaipanda@qq.com>
2026-03-30 17:41:03 -0700

    通渠描述？

--- pkg/panda_one_vs_one1/skills/tongqu.lua
+++ pkg/panda_one_vs_one1/skills/tongqu.lua
@@ -8,7 +8,10 @@ Fk:loadTranslationTable{
   ["#pang__tongqu"] = "通渠：你可以选择将手牌摸至三张或弃牌",
 
   ["@@pang__tongqu-turn"] = "可通渠",
-  ["#pang__tongqu_discard"] = "通渠：点取消将手牌摸至三张，或选择一张牌弃置并于本回合的下个阶段结束时发动此技能",
+  ["#pang__tongqu_discard_draw"] = "通渠：点取消将手牌摸至三张，或选择一张牌弃置并于 出牌阶段 结束时发动此技能",
+  ["#pang__tongqu_discard_play"] = "通渠：点取消将手牌摸至三张，或选择一张牌弃置并 弃牌阶段 结束时发动此技能",
+  ["#pang__tongqu_discard_discard"] = "通渠：点取消将手牌摸至三张，或选择一张牌弃置并于 结束阶段 结束时发动此技能",
+  ["#pang__tongqu_discard_finish"] = "通渠：点取消将手牌摸至三张，或选择一张牌弃置然后无事发生",
 
   ["$pang__tongqu1"] = "兴凿修渠，依水屯军！",
   ["$pang__tongqu2"] = "开渠疏道，以备军实！",
@@ -30,7 +33,10 @@ tongqu:addEffect(fk.EventPhaseEnd, {
     local room = player.room
     local card = room:askToDiscard(player, {
       skill_name = tongqu.name,
-      prompt = "#pang__tongqu_discard",
+      prompt = player.phase == (Player.Draw and "#pang__tongqu_discard_draw" 
+      or player.phase == Player.Play and "#pang__tongqu_discard_play" 
+      or player.phase == Player.Discard and "#pang__tongqu_discard_discard" 
+      or "#pang__tongqu_discard_finish"),
       cancelable = true,
       min_num = 1,
       max_num = 1,
