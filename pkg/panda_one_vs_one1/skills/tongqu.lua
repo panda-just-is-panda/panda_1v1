332bcc9301727acb8a9136c3084897893e7f87b2
胖即是胖 <amaipanda@qq.com>
2026-03-30 17:16:03 -0700

    通渠重做2

--- pkg/panda_one_vs_one1/skills/tongqu.lua
+++ pkg/panda_one_vs_one1/skills/tongqu.lua
@@ -4,16 +4,47 @@ local tongqu = fk.CreateSkill {
 
 Fk:loadTranslationTable{
   ["pang__tongqu"] = "通渠",
-  [":pang__tongqu"] = "摸牌阶段结束时，你将手牌摸至三张，然后你可以弃置一张牌并将此技能下次发动时机改为出牌阶段结束时或准备阶段。",
-  ["#pang__tongqu"] = "通渠：你可以将所有非本回合获得的手牌作为【无中生有】使用。",
+  [":pang__tongqu"] = "摸牌阶段结束时，你可以将手牌摸至三张，然后你可以弃置一张牌并于本回合的下个阶段结束时发动此技能。",
+  ["#pang__tongqu"] = "通渠：你可以将手牌摸至三张",
 
-  ["@@pang__tongqu-inhand-turn"] = "本回合获得",
+  ["@@pang__tongqu-turn"] = "可通渠",
+  ["#pang__tongqu_discard"] = "通渠：你可以弃置一张牌，于本回合的下个阶段结束时再次发动此技能",
 
   ["$pang__tongqu1"] = "兴凿修渠，依水屯军！",
   ["$pang__tongqu2"] = "开渠疏道，以备军实！",
 }
 
-
+tongqu:addEffect(fk.EventPhaseEnd, {
+  anim_type = "drawcard",
+  can_trigger = function(self, event, target, player, data)
+    return target == player and player:hasSkill(tongqu.name) and (player.phase == Player.Draw or player:getMark("@@pang__tongqu-turn") > 0)
+  end,
+  on_cost = function(self, event, target, player, data)
+    player.room:setPlayerMark(player, "@@jutu", 0)
+    return player.room:askToSkillInvoke(player, {
+        skill_name = tongqu.name,
+        prompt = "#pang__tongqu",
+      })
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local X = 3 - player:getHandcardNum()
+    if X > 0 then
+        player:drawCards(X, tongqu.name)
+    end
+    local card = room:askToDiscard(player, {
+      skill_name = tongqu.name,
+      prompt = "#pang__tongqu_discard",
+      cancelable = true,
+      min_num = 1,
+      max_num = 1,
+      include_equip = true,
+    })
+    if #card > 0 then
+      room:setPlayerMark(player, "@@pang__tongqu-turn", 1)
+    end
+  end,
+})
 
 
 return tongqu
